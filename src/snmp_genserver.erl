fd976d5fee638ed029528b1e77ec3993b11ed722
Kreastone <fonora@mail.ru>
2019-05-31 16:20:26 +0500

    v3_1

--- src/snmp_genserver.erl
+++ src/snmp_genserver.erl
@@ -391,8 +391,8 @@ create_file_config(List_Config) ->
       {<<"no auth, no priv">>, _, _} -> {noAuthNoPriv, usmNoAuthProtocol, "", usmNoPrivProtocol, ""};
       {<<"auth, no priv">>, <<"md5">>, _} -> {authNoPriv, usmHMACMD5AuthProtocol, snmp_usm:passwd2localized(md5, AuthKeyString, "telecom"), usmNoPrivProtocol, ""};
       {<<"auth, no priv">>, <<"sha">>, _} -> {authNoPriv, usmHMACSHAAuthProtocol, snmp_usm:passwd2localized(sha, AuthKeyString, "telecom"), usmNoPrivProtocol, ""};
-      {<<"auth, priv">>, <<"md5">>, <<"des">>} -> {authPriv, usmHMACMD5AuthProtocol, snmp_usm:passwd2localized(md5, AuthKeyString, "telecom"), usmDESPrivProtocol, snmp_usm:passwd2localized(md5, "telecom", PrivKeyString)};
-      {<<"auth, priv">>, <<"md5">>, <<"aes">>} -> {authPriv, usmHMACMD5AuthProtocol, snmp_usm:passwd2localized(md5, AuthKeyString, "telecom"), usmDESPrivProtocol, snmp_usm:passwd2localized(md5, "telecom", PrivKeyString)};
+      {<<"auth, priv">>, <<"md5">>, <<"des">>} -> {authPriv, usmHMACMD5AuthProtocol, snmp_usm:passwd2localized(md5, PrivKeyString, "telecom"), usmDESPrivProtocol, snmp_usm:passwd2localized(md5, "telecom", PrivKeyString)};
+      {<<"auth, priv">>, <<"md5">>, <<"aes">>} -> {authPriv, usmHMACMD5AuthProtocol, snmp_usm:passwd2localized(md5, PrivKeyString, "telecom"), usmDESPrivProtocol, snmp_usm:passwd2localized(md5, "telecom", PrivKeyString)};
       {_, _, _, _, _} -> {noAuthNoPriv, usmNoAuthProtocol, "", usmNoPrivProtocol, ""}
     end,
 
